751419ba3153373c6f9e858aea53d5f0a5567474
huangjinhui <279714010@qq.com>
2024-08-20 19:09:00 +0800

    移除swift相关配置

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -34,32 +34,7 @@ Plugin.opts = {
 		'vimdoc',
 		'css',
 		'json',
-		'swift',
 		'go'
 	},
 }
 
-function Plugin.config(name, opts)
-	require('nvim-treesitter.configs').setup(opts)
-	-- 定义 CurrentFunction 函数
-	function CurrentFunction()
-		local node = require 'nvim-treesitter.ts_utils'.get_node_at_cursor()
-		while node do
-			if node:type() == 'function' or node:type() == 'method_declaration' or node:type() == 'function_definition' then
-				return vim.treesitter.get_node_text(node, 0)
-			end
-			node = node:parent()
-		end
-		return "No function"
-	end
-
-	-- 在命令行显示当前函数
-	vim.cmd [[
-    command! ShowCurrentFunction :lua print(CurrentFunction())
-  ]]
-
-	-- 将 ShowCurrentFunction 命令绑定到 <leader>cf 快捷键
-	vim.api.nvim_set_keymap('n', '<leader>cf', ':ShowCurrentFunction<CR>', { noremap = true, silent = true })
-end
-
-return Plugin
