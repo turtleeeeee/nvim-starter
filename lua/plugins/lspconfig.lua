84b22f87354fb798f25f4c8f04b60ea5947066d9
huangjinhui <279714010@qq.com>
2024-10-09 10:57:19 +0800

    go适配

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -59,28 +59,28 @@ function Plugin.config()
 		callback = user.on_attach
 	})
 
-	-- vim.api.nvim_create_autocmd("BufWritePre", {
-	-- 	pattern = "*.go",
-	-- 	callback = function()
-	-- 		local params = vim.lsp.util.make_range_params()
-	-- 		params.context = { only = { "source.organizeImports" } }
-	-- 		-- buf_request_sync defaults to a 1000ms timeout. Depending on your
-	-- 		-- machine and codebase, you may want longer. Add an additional
-	-- 		-- argument after params if you find that you have to write the file
-	-- 		-- twice for changes to be saved.
-	-- 		-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-	-- 		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-	-- 		for cid, res in pairs(result or {}) do
-	-- 			for _, r in pairs(res.result or {}) do
-	-- 				if r.edit then
-	-- 					local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-	-- 					vim.lsp.util.apply_workspace_edit(r.edit, enc)
-	-- 				end
-	-- 			end
-	-- 		end
-	-- 		vim.lsp.buf.format({ async = true })
-	-- 	end
-	-- })
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*.go",
+		callback = function()
+			local params = vim.lsp.util.make_range_params()
+			params.context = { only = { "source.organizeImports" } }
+			-- buf_request_sync defaults to a 1000ms timeout. Depending on your
+			-- machine and codebase, you may want longer. Add an additional
+			-- argument after params if you find that you have to write the file
+			-- twice for changes to be saved.
+			-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+			local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+			for cid, res in pairs(result or {}) do
+				for _, r in pairs(res.result or {}) do
+					if r.edit then
+						local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+						vim.lsp.util.apply_workspace_edit(r.edit, enc)
+					end
+				end
+			end
+			vim.lsp.buf.format({ async = true })
+		end
+	})
 
 	-- See :help mason-lspconfig-settings
 	require('mason-lspconfig').setup({
@@ -90,7 +90,7 @@ function Plugin.config()
 			'html',
 			'cssls',
 			'lua_ls',
-			-- "gopls",
+			"gopls",
 			-- "goimports",
 			"pyright",
 			"sqlls",
@@ -151,11 +151,20 @@ function Plugin.config()
 			-- 		filetypes = { 'toml', 'tpl' },
 			-- 	})
 			-- end,
-			-- ['gopls'] = function()
-			-- 	lspconfig.gopls.setup({
-			-- 		capabilities = lsp_capabilities,
-			-- 	})
-			-- end,
+			['gopls'] = function()
+				lspconfig.gopls.setup({
+					capabilities = lsp_capabilities,
+					settings = {
+						gopls = {
+							-- analyses = {
+							-- 	unusedparams = true,
+							-- },
+							-- staticcheck = true,
+							gofumpt = true,
+						}
+					}
+				})
+			end,
 
 		}
 	})
